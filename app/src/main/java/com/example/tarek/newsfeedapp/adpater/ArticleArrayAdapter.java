bac53294bc3e0a4ce115dc24ea065a9e42675d42
Tarek <tarek.ma91@yahoo.com>
2018-05-26 05:54:56 +0200

    update some values ,

--- app/src/main/java/com/example/tarek/newsfeedapp/adpater/ArticleArrayAdapter.java
+++ app/src/main/java/com/example/tarek/newsfeedapp/adpater/ArticleArrayAdapter.java
@@ -1,3 +1,19 @@
+/*
+Copyright 2018 tarekmabdallah91@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package com.example.tarek.newsfeedapp.adpater;
 
 import android.content.Context;
@@ -20,6 +36,10 @@ public class ArticleArrayAdapter extends ArrayAdapter<Article> {
     private static final String NEW_LINE = "\n";
     private static final int ZERO = 0;
     private static final int MAX_LENGTH_LINE_1 = 11;
+    private static final String T = "T";
+    private static final String Z = "Z";
+    private static final String EMPTY_TEXT = "";
+
 
     public ArticleArrayAdapter(@NonNull Context context, List<Article> articles) {
         super(context, 0 , articles);
@@ -52,16 +72,16 @@ public class ArticleArrayAdapter extends ArrayAdapter<Article> {
     }
 
     private String makeDateInTwoLines (String inputDate){
-        String date  = inputDate.substring(ZERO,MAX_LENGTH_LINE_1); // get first 11 chars to it (Date like 2018-3-13T)
-        String time = inputDate.substring(MAX_LENGTH_LINE_1); // get all remained chars
+        String date = inputDate.substring(ZERO, MAX_LENGTH_LINE_1).replace(T, EMPTY_TEXT); // get first 11 chars to it (Date like 2018-3-13T)
+        String time = inputDate.substring(MAX_LENGTH_LINE_1).replace(Z, EMPTY_TEXT); // get all remained chars
         return date + NEW_LINE + time;
     }
 
 
     private static class ViewHolderItem {
-         TextView titleTextView ;
-         TextView sectionTextView;
-         TextView authorTextView;
-         TextView dateTextView;
+        TextView titleTextView ;
+        TextView sectionTextView;
+        TextView authorTextView;
+        TextView dateTextView;
     }
 }
