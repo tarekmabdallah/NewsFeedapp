7a169d6c945b5d84b2e8cadb964d5f3056c86868
Tarek <tarekmabdallah91@gmail.com>
2019-04-13 18:52:29 +0200

    - use recyclerView instead of list view in the ArticlesFragment - update the ArticleAdapter

--- app/src/main/java/com/example/tarek/news/views/section/articlesFragment/ArticlesFragment.java
+++ app/src/main/java/com/example/tarek/news/views/section/articlesFragment/ArticlesFragment.java
@@ -1,33 +1,33 @@
 package com.example.tarek.news.views.section.articlesFragment;
 
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.tarek.news.R;
 import com.example.tarek.news.models.section.ResponseSection;
 import com.example.tarek.news.models.section.articles.Article;
-import com.example.tarek.news.views.bases.ArticleAdapter;
 import com.example.tarek.news.views.bases.BaseDataLoaderFragment;
+import com.example.tarek.news.views.section.articlesFragment.adapter.ArticleAdapter;
+import com.example.tarek.news.views.section.articlesFragment.adapter.OnArticleClickListener;
 
 import java.util.List;
 
 import butterknife.BindView;
 
+import static com.example.tarek.news.views.section.SectionActivity.openSectionActivity;
 import static com.example.tarek.news.views.webViewActivity.WebViewActivity.openWebViewActivityArticle;
 
 
 public class ArticlesFragment extends BaseDataLoaderFragment {
 
-    @BindView(R.id.list_view)
-    ListView listView;
+    @BindView(R.id.articles_recycler_view)
+    RecyclerView articlesRecyclerView;
 
     protected ArticleAdapter adapter  ;
     protected List<Article> articles ;
 
     protected String sectionId;
 
-    // TODO: 4/13/2019 use recyclerView instead of list view
     // TODO: 4/13/2019 create fragment to open the article with it's data (not the website's link)
 
     @Override
@@ -38,8 +38,8 @@ public class ArticlesFragment extends BaseDataLoaderFragment {
     @Override
     protected void initiateValues() {
         super.initiateValues();
-        adapter = new ArticleAdapter(activity);
-        setListView();
+
+        setArticlesRecyclerView();
     }
 
     @Override
@@ -47,25 +47,39 @@ public class ArticlesFragment extends BaseDataLoaderFragment {
         return sectionId;
     }
 
-    private void setListView() {
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+    private void setArticlesRecyclerView() {
+        OnArticleClickListener onArticleClickListener = new OnArticleClickListener() {
+
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onClickArticle(int position) {
                 Article article = articles.get(position);
                 openWebViewActivityArticle(activity, article.getWebUrl());
             }
-        });
+
+            @Override
+            public void onClickArticleSection(int position) {
+                Article article = articles.get(position);
+                String sectionName = article.getSectionName();
+                String activityTitle = activity.getTitle().toString();
+                if (!activityTitle.equals(sectionName))
+                    openSectionActivity(activity, article.getSectionId(), article.getSectionName());
+            }
+        };
+        adapter = new ArticleAdapter(activity);
+        adapter.setOnArticleClickListener(onArticleClickListener);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(activity);
+        articlesRecyclerView.setLayoutManager(layoutManager);
+        articlesRecyclerView.setHasFixedSize(true);
+        articlesRecyclerView.setAdapter(adapter);
     }
 
     @Override
     protected void whenDataFetchedGetResponse(Object response) {
-        adapter.clear();
         if (response instanceof ResponseSection) {
             ResponseSection section = (ResponseSection) response;
             List<Article> articleList = section.getResponse().getItems();
             if (articleList != null && !articleList.isEmpty()) {
-                adapter.addAll(articleList);
+                adapter.swapList(articleList);
             } else handleNoDataFromResponse();
         }
     }
---
+++ app/src/main/java/com/example/tarek/news/views/section/articlesFragment/adapter/OnArticleClickListener.java
@@ -0,0 +1,13 @@
+package com.example.tarek.news.views.section.articlesFragment.adapter;
+
+public interface OnArticleClickListener {
+    /**
+     * to open the details of the clicked article (by position)
+     */
+    void onClickArticle(int position);
+
+    /**
+     * to open the section of the clicked article
+     */
+    void onClickArticleSection(int position);
+}
